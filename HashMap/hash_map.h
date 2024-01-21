f8ae8fb795763054198c9d8e785df8c6b8a28187
HHayk004 <hayk.hovsepyan004@gmail.com>
2024-01-21 13:16:03 +0400

    Changing HashMap for briefity:

--- HashMap/hash_map.h
+++ HashMap/hash_map.h
@@ -6,17 +6,21 @@
     #include <vector>
     #include <list>
 
+    template <typename T>
+    size_t hash(T key, size_t size);
+
+    template <>
+    size_t hash(std::string, size_t size);
+
     template <typename T, typename U>
     class HashMap {
         private:
-            size_t nextSize() const;
-
             std::vector<size_t> m_prime;
             std::vector<std::pair<size_t, std::list< std::pair<T, U> > > > m_vec; // first element is size of list
 
         private:
-            size_t hash(T key, size_t size) const;
-
+            size_t nextSize() const;
+            
         public:
             HashMap();
             ~HashMap() = default;
@@ -40,43 +44,7 @@
             std::vector<T> keys() const;
             std::vector<U> values() const;
             std::vector<std::pair<T, U> > pairs() const;
-    };    
-
-    template <typename U>
-    class HashMap<std::string, U> {
-        private:
-            size_t nextSize() const;
-
-            std::vector<size_t> m_prime;
-            std::vector<std::pair<size_t, std::list< std::pair<std::string, U> > > > m_vec; // first element is size of list
-
-        private:
-            size_t hash(std::string key, size_t size) const;
-
-        public:
-            HashMap();
-            ~HashMap() = default;
-
-            void insert(std::pair<std::string, U>);
-            void erase(std::string key);
-
-            void rehash();
-
-            U& get(std::string key);
-
-            void clear();
-
-            size_t size() const;
-            
-            bool empty() const;
-    
-            bool containsKey(std::string key) const;
-            bool containsValue(U val) const;
-
-            std::vector<std::string> keys() const;
-            std::vector<U> values() const;
-            std::vector<std::pair<std::string, U> > pairs() const;
-    };   
+    };      
 
     #include "hash_map.hpp"
 
--- HashMap/hash_map.hpp
+++ HashMap/hash_map.hpp
@@ -124,12 +124,6 @@ size_t HashMap<T, U>::nextSize() const
 }
 
 template <typename T, typename U>
-size_t HashMap<T, U>::hash(T key, size_t size) const
-{
-    return static_cast<size_t>(key) % size; 
-}
-
-template <typename T, typename U>
 size_t HashMap<T, U>::size() const
 {
     size_t result = 0;
@@ -139,6 +133,26 @@ size_t HashMap<T, U>::size() const
     }
 
     return result;
+}
+
+template <typename T>
+size_t hash(T key, size_t size)
+{
+    return static_cast<size_t>(key) % size; 
+}
+
+template <>
+size_t hash(std::string key, size_t size)
+{
+    const size_t prime_const = 31; // can be any number
+    
+    size_t result = 0;
+    for (size_t i = 0; i < key.size(); ++i)
+    {
+        result += key[i] * std::pow(prime_const, i);
+    }
+
+    return result % size;
 }
 
 template <typename T, typename U>
@@ -232,247 +246,5 @@ std::vector<std::pair<T, U> > HashMap<T, U>::pairs() const
 
     return result;
 }
-
-template <typename U>
-HashMap<std::string, U>::HashMap()
-{
-    m_prime = {2, 3};
-    m_vec.resize(m_prime.back());
-}
-
-template <typename U>
-void HashMap<std::string, U>::insert(std::pair<std::string, U> elem)
-{
-    size_t index = hash(elem.first, m_vec.size());
-
-    for (auto it = m_vec[index].second.begin(); it != m_vec[index].second.end(); ++it)
-    {
-        if (it->first == elem.first)
-        {
-            if (it->second != elem.second)
-            {
-                it->second = elem.second;
-            }
-
-            return;
-        }
-    }
-    
-    m_vec[index].second.push_front(elem);
-    ++m_vec[index].first;
-
-    if (m_vec[index].first > m_vec.size())
-    {
-        rehash();
-    }
-}
-
-template <typename U>
-void HashMap<std::string, U>::erase(std::string key)
-{
-    size_t index = hash(key, m_vec.size());
-
-    for (auto it = m_vec[index].second.begin(); it != m_vec[index].second.end(); ++it)
-    {
-        if (it->first == key)
-        {
-            m_vec[index].second.erase(it);
-            break;
-        }
-    }
-}
-
-template <typename U>
-void HashMap<std::string, U>::rehash()
-{
-    std::vector<std::pair<size_t, std::list<std::pair<std::string, U> > > > tmp;
-    tmp.resize(nextSize());
-
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        for (auto it = m_vec[i].second.begin(); it != m_vec[i].second.end(); ++it)
-        {
-            size_t index = hash(it->first, tmp.size());
-
-            tmp[index].second.push_front(*it);
-            ++tmp[index].first;
-        }
-    }
-
-    m_vec = std::move(tmp);
-}
-
-template <typename U>
-U& HashMap<std::string, U>::get(std::string key)
-{
-    const size_t index = hash(key, m_vec.size());
-
-    for (auto it = m_vec[index].second.begin(); it != m_vec[index].second.end(); ++it)
-    {
-        if (it->first == key)
-        {
-            return it->second;
-        }
-    }
-
-    std::cerr << "std::stringhe element doesn't exist!\n";
-    exit(1);
-}
-
-template <typename U>
-void HashMap<std::string, U>::clear()
-{
-    m_vec.clear();
-}
-
-template <typename U>
-size_t HashMap<std::string, U>::nextSize() const
-{
-    size_t num = m_prime.back() + 2;
-
-    while (true)
-    {
-        size_t val = std::sqrt(num);
-
-        bool check = true;
-        for (int i = 0; i < m_prime.size() && m_prime[i] <= val; ++i)
-        {
-            if (num % m_prime[i] == 0)
-            {
-                check = false;
-                break;
-            }
-        }
-
-        if (check)
-        {
-            break;
-        }
-
-        num += 2;
-    }
-
-    return num;
-}
-
-template <typename U>
-size_t HashMap<std::string, U>::hash(std::string key, size_t size) const
-{
-    const size_t prime_const = 31; // can be any number
-
-    size_t result = 0;
-    for (int i = 0; i < key.size(); ++i)
-    {
-        result += key[i] * pow(prime_const, i);
-    }
-
-    return result % size;
-}
-
-template <typename U>
-size_t HashMap<std::string, U>::size() const
-{
-    size_t result = 0;
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        result += m_vec[i].first;
-    }
-
-    return result;
-}
-
-template <typename U>
-bool HashMap<std::string, U>::empty() const
-{
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        if (m_vec[i].first)
-        {
-            return false;
-        }
-    }
- 
-    return true;
-}
-
-template <typename U>
-bool HashMap<std::string, U>::containsKey(std::string key) const
-{
-    size_t index = hash(key);
-
-    for (auto it = m_vec[index].second.begin(); it != m_vec[index].second.end(); ++it)
-    {
-        if (it->first == key)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-template <typename U>
-bool HashMap<std::string, U>::containsValue(U val) const
-{
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        for (auto it = m_vec[i].second.begin(); it != m_vec[i].second.end(); ++it)
-        {
-            if (it->second == val)
-            {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-template <typename U>
-std::vector<std::string> HashMap<std::string, U>::keys() const
-{
-    std::vector<std::string> result;
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        for (auto it = m_vec[i].second.begin(); it != m_vec[i].second.end(); ++it)
-        {
-            result.push_back(it->first);
-        }
-    }
-
-    return result;
-}
-
-template <typename U>
-std::vector<U> HashMap<std::string, U>::values() const
-{
-    std::vector<U> result;
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        for (auto it = m_vec[i].second.begin(); it != m_vec[i].second.end(); ++it)
-        {
-            result.push_back(it->second);
-        }
-    }
-
-    return result;
-}
-
-template <typename U>
-std::vector<std::pair<std::string, U> > HashMap<std::string, U>::pairs() const
-{
-    std::vector<std::pair<std::string, U> > result;
-    for (int i = 0; i < m_vec.size(); ++i)
-    {
-        for (auto it = m_vec[i].second.begin(); it != m_vec[i].second.end(); ++it)
-        {
-            result.push_back(*it);
-        }
-    }
-
-    return result;
-}
-
-
 
 #endif
